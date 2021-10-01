1be403440c084655bd3e65c5af39c8770165139f
Paulo Pereira <tec.prpereira@gmail.com>
2021-10-01 00:17:28 -0300

    Created 'edit' form file

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,7 +2,27 @@
 
 @section('title', 'Edit Tasks')
 
+@section('title', 'Tasks > Form')
+
 @section('content')
-    Passou pela rota e pelo controller. <br/>
-    A task a ser editada é a: {{$id}}
+    <fieldset>
+        <legend>Edit task</legend>
+        <form method="POST">
+            @csrf
+
+            <div>
+                <label for="title">Task Title</label>
+                <input type="text" name="title" id="title" maxlength="100" value='{{ $item->title }}' />
+
+                @if (session('inputError'))
+                    @inputError
+                    {{ session('inputError') }}
+                    @endinputError
+                @endif
+            </div>
+
+            <button type="submit">Update task</button>
+        </form>
+    </fieldset>
+
 @endsection

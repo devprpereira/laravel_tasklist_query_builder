60df62f0abe1d1cf33fc0938fb3dddb3ef3220ec
Paulo Pereira <tec.prpereira@gmail.com>
2021-10-01 00:17:17 -0300

    Created 'add' form file

--- resources/views/tasks/add.blade.php
+++ resources/views/tasks/add.blade.php
@@ -1,15 +1,26 @@
 @extends('layouts.tasks')
 
-@section('title',  'Tasks > Add')
+@section('title', 'Tasks > Form')
 
 @section('content')
-<form method="POST" action="">
-    @csrf
-    <label for="title">Task Title</label>
-    <input type="text" name="title" id="title"/>
+    <fieldset>
+        <legend>Add task form</legend>
+        <form method="POST">
+            @csrf
 
-    <input type="submit" value="Add task">
+            <div>
+                <label for="title">Task Title</label>
+                <input type="text" name="title" id="title" maxlength="100" />
 
-</form>
+                @if (session('inputError'))
+                    @inputError
+                    {{ session('inputError') }}
+                    @endinputError
+                @endif
+            </div>
+
+            <button type="submit">Add task</button>
+        </form>
+    </fieldset>
 
 @endsection
